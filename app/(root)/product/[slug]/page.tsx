cda0bc47312461023f47687798ceb0720451a718
josephori <ori100no@gmail.com>
2025-05-30 21:12:38 +0300

    added images component for product page

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -4,6 +4,7 @@ import { Card, CardContent } from "@/components/ui/card";
 import { getProductBySlug } from "@/lib/actions/products.actions";
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
+import ProductImages from "@/components/shared/product/product-images";
 
 const ProductDetailsPage = async (props: { params: Promise<{ slug: string }> }) => {
   const params = await props.params;
@@ -17,7 +18,9 @@ const ProductDetailsPage = async (props: { params: Promise<{ slug: string }> }) 
     <section>
       <div className="grid grid-cols-1 md:grid-cols-5">
         {/* Images Column */}
-        <div className="col-span-2">{/* TODO: Add Images component */}</div>
+        <div className="col-span-2">
+          <ProductImages images={product.images} />
+        </div>
 
         {/* Details Column */}
         <div className="col-span-2 p-5">
