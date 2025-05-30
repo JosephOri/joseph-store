5fdb44960ca44badff10f240121f915dcf4f6304
josephori <ori100no@gmail.com>
2025-05-30 15:14:40 +0300

    refactor

--- lib/validators.ts
+++ lib/validators.ts
@@ -9,13 +9,13 @@ const currency = z
   );
 
 export const insertProductSchema = z.object({
-  name: z.string().min(3, "Name must be at least 3 characters long"),
-  slug: z.string().min(3, "Slug must be at least 3 characters long"),
-  category: z.string().min(3, "Category must be at least 3 characters long"),
-  brand: z.string().min(3, "Brand must be at least 3 characters long"),
-  description: z.string().min(3, "Description must be at least 3 characters long"),
+  name: z.string().min(3, "Name must be at least 3 characters"),
+  slug: z.string().min(3, "Slug must be at least 3 characters"),
+  category: z.string().min(3, "Category must be at least 3 characters"),
+  brand: z.string().min(3, "Brand must be at least 3 characters"),
+  description: z.string().min(3, "Description must be at least 3 characters"),
   stock: z.coerce.number(),
-  images: z.array(z.string()).min(1, "At least one image is required in product"),
+  images: z.array(z.string()).min(1, "Product must have at least one image"),
   isFeatured: z.boolean(),
   banner: z.string().nullable(),
   price: currency,
