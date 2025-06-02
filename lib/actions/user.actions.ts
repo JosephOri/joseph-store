3078ecd7d4d7ccc5e29a9b3bee76a639e31197b0
josephori <ori100no@gmail.com>
2025-06-02 16:36:13 +0300

    improved error handing

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -6,11 +6,9 @@ import { isRedirectError } from "next/dist/client/components/redirect-error";
 import { signUpFormSchema } from "@/lib/validators";
 import { hashSync } from "bcrypt-ts-edge";
 import { prisma } from "@/db/prisma";
+import { formatError } from "@/lib/utils";
 
-export async function signInWithCredentials(
-  prevState: unknown,
-  data: FormData,
-) {
+export async function signInWithCredentials(prevState: unknown, data: FormData) {
   try {
     const user = signInFormSchema.parse({
       email: data.get("email") || "",
@@ -70,7 +68,7 @@ export async function signUpUser(prevState: unknown, formData: FormData) {
     }
     return {
       success: false,
-      message: "Failed to sign up user",
+      message: formatError(error),
     };
   }
 }
