4de554227dcf4737ceb15859a27b181f206aadbf
josephori <ori100no@gmail.com>
2025-06-02 14:05:24 +0300

    added login functionality

--- app/(auth)/sign-in/credentials-signin-form.tsx
+++ app/(auth)/sign-in/credentials-signin-form.tsx
@@ -5,10 +5,23 @@ import { Input } from "@/components/ui/input";
 import { signInDefaultValues } from "@/lib/constants";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { signInWithCredentials } from "@/lib/actions/user.actions";
+import { useActionState } from "react";
+import { useFormStatus } from "react-dom";
 
 const CredentialsSignInForm = () => {
+  const [data, action] = useActionState(signInWithCredentials, { success: false, message: "" });
+
+  const SignInButton = () => {
+    const { pending } = useFormStatus();
+    return (
+      <Button className="w-full" variant="default" disabled={pending}>
+        {pending ? "Signing in..." : "Sign In"}
+      </Button>
+    );
+  };
   return (
-    <form>
+    <form action={action}>
       <div className="space-y-6">
         <div>
           <Label htmlFor="email">Email</Label>
@@ -34,11 +47,8 @@ const CredentialsSignInForm = () => {
             defaultValue={signInDefaultValues.password}
           />
         </div>
-        <div>
-          <Button className="w-full" variant="default">
-            Sign In
-          </Button>
-        </div>
+        <SignInButton />
+        {data && !data.success && <div className="text-center text-destructive">{data.message}</div>}
         <div className="text-sm text-center text-muted-foreground">
           Don&apos;t have an account?{" "}
           <Link href="/sign-up" className="text-primary">
