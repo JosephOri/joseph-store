242c8daaf1f088ef43a8fd40b53a397397b0db7f
josephori <ori100no@gmail.com>
2025-06-07 12:44:00 +0300

    Add protected routes check in auth middleware
    Implement path protection for authenticated routes by adding regex pattern matching. When unauthenticated users attempt to access protected paths like /profile or /admin, the middleware now returns false to deny access. This enhances security by preventing unauthorized access to sensitive routes.

--- auth.ts
+++ auth.ts
@@ -101,6 +101,23 @@ export const config = {
     },
     // eslint-disable-next-line
     authorized({ request, auth }: any) {
+      // Array of regex patterns of protected paths
+      const protectedPaths = [
+        /\/shipping-address/,
+        /\/payment-method/,
+        /\/place-order/,
+        /\/profile/,
+        /\/user\/(.*)/,
+        /\/order\/(.*)/,
+        /\/admin/,
+      ];
+
+      // Get pathname from the req URL object
+      const { pathname } = request.nextUrl;
+
+      // Check if user is not authenticated and on a protected path
+      if (!auth && protectedPaths.some((p) => p.test(pathname))) return false;
+
       if (!request.cookie?.get("sessionCartId")) {
         const sessionCartId = crypto.randomUUID();
         const newRequestHeaders = new Headers(request.headers);
