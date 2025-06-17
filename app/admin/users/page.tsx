33b9c1693d42e253c552a6a84fe6fe2fab2928cd
josephOri <ori100no@gmail.com>
2025-06-17 15:21:26 +0300

    styled badge

--- app/admin/users/page.tsx
+++ app/admin/users/page.tsx
@@ -51,6 +51,7 @@ const AdminUserPage = async (props: {
                 <TableCell>{user.email}</TableCell>
                 <TableCell>
                   <Badge
+                    className="text-md rounded-full px-3 py-1"
                     variant={user.role === "admin" ? "default" : "outline"}
                   >
                     {user.role}
